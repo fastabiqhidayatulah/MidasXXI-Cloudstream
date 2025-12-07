87f702724c1e5318d3d229f387a1e68f4391fa1e
fastabiqhidayatulah <80563540+fastabiqhidayatulah@users.noreply.github.com>
2025-12-07 14:56:36 +0700

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+cat > settings.gradle.kts << 'EOF'
+rootProject.name = "MidasXXI"
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+EOF
